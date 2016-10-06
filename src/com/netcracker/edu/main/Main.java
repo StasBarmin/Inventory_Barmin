78ba29d275a000709af02fe7bcea96da030dad92
barmin <barminstas@gmail.com>
2016-10-06 14:30:15 +0300

    Sort

--- src/com/netcracker/edu/main/Main.java
+++ src/com/netcracker/edu/main/Main.java
@@ -3,6 +3,15 @@ package com.netcracker.edu.main;
 public class Main {
 
     public static void main(String[] args) {
-        //TODO something
+        print(args);
+    }
+    public static void print(String[] args) {
+        for(int i=0; i<args.length; i++){
+            System.out.println(args[i]);
+        }
+    }
+
+    public static void sort(String[] args) {
+        
     }
 }
