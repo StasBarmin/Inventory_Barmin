bb5f6cdfeaa850cd11a4eb98162340bcaccebe2e
barmin <barminstas@gmail.com>
2016-10-07 12:34:52 +0300

    Sort

--- src/com/netcracker/edu/main/Main.java
+++ src/com/netcracker/edu/main/Main.java
@@ -8,18 +8,20 @@ public class Main {
         sort(args);
         print(args);
     }
+
     public static void print(String[] args) {
-        for(int i=0; i<args.length; i++){
+        for (int i = 0; i < args.length; i++){
             System.out.println(args[i]);
         }
     }
 
     public static void sort(String[] args) {
         boolean flag = true;
-        while(flag){
-            flag=false;
-            for(int i=0; i<args.length-1; i++){
-                if(args[i].compareTo(args[i+1])>0) {
+
+        while (flag){
+            flag = false;
+            for (int i = 0; i < args.length - 1; i++){
+                if (args[i].compareTo(args[i + 1]) > 0) {
                     String temp = args[i + 1];
                     args[i + 1] = args[i];
                     args[i] = temp;
